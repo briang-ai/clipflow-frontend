ef35763f9f71654a491b26f31f7020abc3ed63b4
Brian Galietti <briangalietti@gmail.com>
2026-03-06 10:41:54 -0700

    Clean up Clerk provider config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 export const dynamic = "force-dynamic";
+
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      afterSignInUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL}
-      afterSignUpUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL}
+      fallbackRedirectUrl="/uploads"
     >
       <html lang="en">
         <body>{children}</body>
