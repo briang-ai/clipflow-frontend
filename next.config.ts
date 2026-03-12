329ada5d871adb73adc8bd6ebefd1ad5cc72d49d
Brian Galietti <briangalietti@gmail.com>
2026-03-12 14:38:54 -0700

    redirect /join to /sign-up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/join",
+        destination: "/sign-up",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
